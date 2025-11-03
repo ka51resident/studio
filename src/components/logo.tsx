5fe4a32746caf2f0af1599a408551b94ab3956a2
Akash Nayak <akash390@gmail.com>
2025-11-03 13:50:55 +0000

    not working fix it

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import logo from './logo.png';
 
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2" aria-label="Akash Enterprises Home">
       <Image
-        src="/logo.png"
+        src={logo}
         alt="Akash Enterprises Logo"
         width={32}
         height={32}
